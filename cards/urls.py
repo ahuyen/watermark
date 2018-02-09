9eadfa0e1c26a105d248171683b1f43ee32f38ff
Ryan Constantino <ryan.constantino93@gmail.com>
2018-02-09 15:25:09 -0800

    Add registration page. Add registration form.

--- cards/urls.py
+++ cards/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name='cards'
 urlpatterns = [
     path('', views.SplashView.as_view(), name='splash'),
-    path('login', views.LoginView.as_view(), name='login')
+    path('login', views.LoginView.as_view(), name='login'),
+    path('signup', views.register, name='register')
 ]
--- cards/views.py
+++ cards/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.views.generic.base import TemplateView
 from django.views import generic
+from .forms import UserCreationForm
+from django.http import HttpResponseRedirect
 
 class SplashView(TemplateView):
     template_name = "cards/splash.html"
@@ -8,5 +10,19 @@ class SplashView(TemplateView):
 class LoginView(TemplateView):
     template_name = "cards/login.html"
 
+class RegisterView(TemplateView):
+    template_name = "cards/register.html"
+
 class WalletView(generic.ListView):
     template_name = "cards/wallet.html"
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            print(form.cleaned_data)
+            return HttpResponseRedirect('/')
+    else:
+        form = UserCreationForm()
+
+    return render(request, 'cards/register.html', {'form' : form})
